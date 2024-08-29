01471239951cb143d5e166bf8eb40cdadba0d9a6
Johnny Michael Borges Carvalho <johnny.michael.b.carvalho@gmail.com>
2024-08-29 13:53:23 -0300

    Adiciona função criarDivTitle para criar e adicionar dinamicamente um <div> com texto ao elemento title no HTML.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 'use strict';
 
-const criarDiv = (texto) => {  //função que recebe texto como parametro  
-    const div = document.createElement('div');//cria um novo elemento <div> e armazena na 'const' div
-    div.classList.add('key');//Adiciona a classe 'key' ao elemento <div>
-    div.textContent = texto;//define  o conteudo do texto da <div>como valor de parametro 'texto'
-    div.id = texto;//atributo da div como o valor de parametro texto
-    document.getElementById('container').appendChild(div)//Adiciona o <div> criado como um filho do elemento com o id container no documento HTML.
+const criarDiv = (texto) => {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('container').appendChild(div);
+
 }
-criarDiv('A');
+criarDiv('A')
+
+const criarDivTitle = (texto) => {
+const div = document.createElement('div');
+div.classList.add('title__main');
+div.textContent = texto;
+div.id = texto;
+document.getElementById('title').appendChild(div);
+
+}
+criarDivTitle('Louvai ao Senhor!')
+
+
+ 
