c7b03bb7ed8dab2258ad704e3722500f63419fcb
Johnny Michael Borges Carvalho <johnny.michael.b.carvalho@gmail.com>
2024-08-30 12:00:34 -0300

    Criação do objeto 'sons' com pares de chave-valor representando letras e arquivos de som, e da função 'letras' para iterar sobre as chaves do objeto e chamar 'criarDiv' para cada chave.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const sons = {
+    'A': 'bom.wav',
+    'S': 'clap.wav',
+    'D': 'hihat.wav',
+    'F': 'kick.wav',
+    'G': 'openhat.wav',
+    'H': 'ride.wav',
+    'J': 'snare.wav',
+    'K': 'tink.wav',
+    'L': 'tom.wav'
+}
+
 const criarDiv = (texto) => {
     const div = document.createElement('div');
     div.classList.add('key');
@@ -8,17 +20,20 @@ const criarDiv = (texto) => {
     document.getElementById('container').appendChild(div);
 
 }
-criarDiv('A')
-
+ 
 const criarDivTitle = (texto) => {
-const div = document.createElement('div');
-div.classList.add('title__main');
-div.textContent = texto;
-div.id = texto;
-document.getElementById('title').appendChild(div);
+    const div = document.createElement('div');
+    div.classList.add('title__main');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('title').appendChild(div);
 
 }
 criarDivTitle('Louvai ao Senhor!')
 
+const letras = (sons) => {
+    Object.keys(sons).forEach(criarDiv);
 
- 
+}
+letras(sons)
+
